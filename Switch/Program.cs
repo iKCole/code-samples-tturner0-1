d47c0e340f942f45bd458b9d78f16fbe4c946980
Tiandre' Turner <tjtics@ad.rit.edu>
2020-02-05 17:40:16 -0500

    Added ternary and switch

---
+++ Switch/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int var1 = 24;
+            int var2 = 99;
+            bool decision = !(!(var1 > 30) || !(var2 < 12 || var1 <= 25));
+            Console.WriteLine(decision);
+            // Console.Write("Enter an letter: ");
+            // string letter = Console.ReadLine().ToUpper();
+            // switch(letter)
+            // {
+            //     case "A":
+            //         Console.WriteLine("You selected an A");
+            //         break;
+            //     case "B":
+            //         Console.WriteLine("You selected a B");
+            //         break;
+            //     default:
+            //         Console.WriteLine("Letter is not found");
+            //         break;
+            // }
+
+            // Console.Write("Enter an number: ");
+            // int myInt = int.Parse(Console.ReadLine());
+
+            // switch(myInt)
+            // {
+            //     case 1: 
+            //     case 2: Console.WriteLine("You selected a A or B");
+            //         break;
+            //     default:
+            //         Console.WriteLine("Letter is not found");
+            //         break;
+            // }
+            //  Console.Write("Enter your grade: ");
+            // double grade = double.Parse(Console.ReadLine());
+            
+            // if (grade >= 90) 
+            // {
+            //     Console.WriteLine("You got an A");
+            // } 
+            // else if (grade >= 80)
+            // { 
+            //         Console.WriteLine("You got a B");
+               
+            // }
+            //  else if (grade >= 70)
+            // {
+            //     Console.WriteLine("You got a C");
+                
+            // }
+            // else if(grade >= 60)
+            // {
+            //      Console.WriteLine("You got a D");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("You got an F");
+            // }
+
+
+
+        }
+    }
+}
---
+++ Ternary/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace Ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number: ");
+            bool isValid = int.TryParse(Console.ReadLine(), out int newNumber);
+
+            if(isValid)
+            {
+
+            }
+            else
+            {
+                
+            }
+            
+
+
+            Console.Write("Welcome to the Zodiac game!\n\n");
+            Console.WriteLine("What is your birth month?: ");
+            string input = Console.ReadLine();
+
+            int number;
+
+            bool isNumber = int.TryParse(input, out number);
+
+            if(isNumber)
+            {
+                switch(number)
+                {
+                    case 1:
+                        Console.WriteLine("You're an Aquarius");
+                        break;
+                    case 2:
+                        Console.WriteLine("You're a Pisces");
+                        break;
+                    case 3:
+                        Console.WriteLine("You're an Aries");
+                        break;
+                    case 4:
+                        Console.WriteLine("You're a Taurus");
+                        break;
+                    case 5:
+                        Console.WriteLine("You're a Gemini");
+                        break;
+                    case 6:
+                        Console.WriteLine("You're a Cancer");
+                        break;
+                    case 7:
+                        Console.WriteLine("You're a Leo");
+                        break;
+                    case 8:
+                        Console.WriteLine("You're a Virgo");
+                        break;
+                    case 9:
+                        Console.WriteLine("You're a Libra");
+                        break;
+                    case 10:
+                        Console.WriteLine("You're a Scoripo");
+                        break;
+                    case 11:
+                        Console.WriteLine("You're a Sagittarius");
+                        break;
+                    case 12:
+                        Console.WriteLine("You're a Capricorn");
+                        break;
+                }
+     
+            }
+            else
+            {
+                string sign;
+                string sign2 = (input == "January") ? "Aquarius" : (input == "March" ? "Aries" : "Pisces");
+
+                if(input == "January")
+                {
+                    sign = "Aqua";
+                }
+                else if(input == "March")
+                {
+                    sign = "Aries";
+                }
+                else
+                    sign = "Pisces";
+
+                Console.WriteLine($"You're a {sign} || {sign2}");
+                // switch(input)
+                // {
+                //     case "January":
+                //         Console.WriteLine("You're an Aquarius");
+                //         break;
+                //     case "February":
+                //         Console.WriteLine("You're a Pisces");
+                //         break;
+                //     case "March":
+                //         Console.WriteLine("You're an Aries");
+                //         break;
+                //     case "April":
+                //         Console.WriteLine("You're a Taurus");
+                //         break;
+                //     case "May":
+                //         Console.WriteLine("You're a Gemini");
+                //         break;
+                //     case "June":
+                //         Console.WriteLine("You're a Cancer");
+                //         break;
+                //     case "July":
+                //         Console.WriteLine("You're a Leo");
+                //         break;
+                //     case "August":
+                //         Console.WriteLine("You're a Virgo");
+                //         break;
+                //     case "September":
+                //         Console.WriteLine("You're a Libra");
+                //         break;
+                //     case "October":
+                //         Console.WriteLine("You're a Scoripo");
+                //         break;
+                //     case "November":
+                //         Console.WriteLine("You're a Sagittarius");
+                //         break;
+                //     case "December":
+                //         Console.WriteLine("You're a Capricorn");
+                //         break;
+                // }
+            }
+
+
+            
+
+
+        }
+    }
+}
