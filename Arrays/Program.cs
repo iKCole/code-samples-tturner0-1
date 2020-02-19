466c00c602f36f96e9df56dbcae4e048f2d50bb8
Tiandre' Turner <tjtics@ad.rit.edu>
2020-02-19 16:51:26 -0500

    Updated code to explain more about what to do for the ICE

--- Arrays/Program.cs
+++ Arrays/Program.cs
@@ -7,42 +7,53 @@ namespace Arrays
         static void Main(string[] args)
         {
             //const int CLASS_SIZE = 7;
+            const string ASHLEY = "Ashley";
+            const string ASHLEY_FAV_POP = "Coke";
+            const double ASHLEY_FAV_POP_PRICE = 2.80;
             const int MAX_CLASS_SIZE = 15;
             string[] classNameArray2 = new string[0]; // create an empty array of string
-            string[] classNameArray = { "Ashley", "Hysuon", 
+            string[] classNameArray = { ASHLEY, "Hysuon", 
                 "Issac", "Asido", "Juliet", "William", "Prof. T ", "Ghost"}; // create and initialize the string array with seven names.
 
-            Console.WriteLine(classNameArray[0] + " " + classNameArray[1] + " " + classNameArray[2] + " " + classNameArray[3] + " " + classNameArray[4] + " " +
-                               classNameArray[5] + " " + classNameArray[6] );
+            string[] studentsFavDrinks = new string[MAX_CLASS_SIZE];
+            studentsFavDrinks[0] = ASHLEY_FAV_POP;
+
+            double[] pricesArray = new double[MAX_CLASS_SIZE];
+            pricesArray[0] = ASHLEY_FAV_POP_PRICE;
+
+            // Console.WriteLine(classNameArray[0] + " " + classNameArray[1] + " " + classNameArray[2] + " " + classNameArray[3] + " " + classNameArray[4] + " " +
+            //                    classNameArray[5] + " " + classNameArray[6] );
+            
+            Console.WriteLine(classNameArray[0] + $"'s favorite pop, {studentsFavDrinks[0]} cost {pricesArray[0]:C}");
 
             // change the values in specific locations
-            classNameArray[0] = "Loco";
-            classNameArray[1] = "Hy";
-            classNameArray[2] = "Ike";
-            classNameArray[3] = "He is not here";
-            classNameArray[4] = "Jule";
-            classNameArray[5] = "Will";
+            // classNameArray[0] = "Loco";
+            // classNameArray[1] = "Hy";
+            // classNameArray[2] = "Ike";
+            // classNameArray[3] = "He is not here";
+            // classNameArray[4] = "Jule";
+            // classNameArray[5] = "Will";
 
-             Console.WriteLine(classNameArray[0] + " " + classNameArray[1] + " " + classNameArray[2] + " " + classNameArray[3] + " " + classNameArray[4] + " " +
-                               classNameArray[5] + " " + classNameArray[6] );
+            //  Console.WriteLine(classNameArray[0] + " " + classNameArray[1] + " " + classNameArray[2] + " " + classNameArray[3] + " " + classNameArray[4] + " " +
+            //                    classNameArray[5] + " " + classNameArray[6] );
 
 
-            Console.WriteLine($"There are {classNameArray.Length} people in this room");
+            // Console.WriteLine($"There are {classNameArray.Length} people in this room");
 
-            // one of the examples of how to use the length for if statements
-            if(classNameArray.Length < MAX_CLASS_SIZE)
-            {
-                Console.WriteLine("There is enough space in the classroom");
-            }
-            else
-            {
-                Console.WriteLine("The classroom is full");
-            }
+            // // one of the examples of how to use the length for if statements
+            // if(classNameArray.Length < MAX_CLASS_SIZE)
+            // {
+            //     Console.WriteLine("There is enough space in the classroom");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("The classroom is full");
+            // }
 
-            if(classNameArray.Length > MAX_CLASS_SIZE)
-            {
-                Console.WriteLine("Need to find another room");
-            }
+            // if(classNameArray.Length > MAX_CLASS_SIZE)
+            // {
+            //     Console.WriteLine("Need to find another room");
+            // }
             
         }
     }
