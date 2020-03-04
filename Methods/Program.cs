c574ad91c2f9c4d91250794af4f7287dff28c51e
Tiandre' Turner <tjtics@ad.rit.edu>
2020-03-04 15:45:02 -0500

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 5; i++)
+            {
+                HelloWorld();
+            }
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Hello(name);
+
+            Console.WriteLine($"Number: {GetNumber()}");
+            Console.WriteLine($"Number: {GetNumber(5)}");
+           
+            // HelloWorld();
+            // HelloWorld();
+            // HelloWorld();
+            // HelloWorld();
+
+            void HelloWorld()
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+
+        // Method
+        // public static void HelloWorld()
+        // {
+        //     Console.WriteLine("Hello World!");
+        // }
+
+        public static void Hello(string name)
+        {
+            Console.WriteLine($"Hello {name}!");
+        }
+
+        public static void Hello(string name, int age)
+        {
+            Console.WriteLine($"Hello {name}, you're {age} years old!");
+        }
+
+         public static void Hello(string name, string age)
+        {
+            Console.WriteLine($"Hello {name}, you're {age} years old!");
+        }
+
+        public static int GetNumber()
+        {
+            return 11;
+        }
+
+        public static int GetNumber(int number)
+        {
+            return 11 * number;
+        }
+    }
+}
